2292a9f5077ade711ce4adb7b712c6c22aade527
Jefferson Clebson <jeffersonclebson@gmail.com>
2021-07-17 11:19:11 -0300

    Add Dark Mode feature

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from "styled-components";
+
+export const light: DefaultTheme = {
+  name: "light",
+  colors: {
+    primaryColors: {
+      primary: "#835AFD",
+      black: "#29292E",
+      danger: "#E73F5D",
+      shadow: "#050206",
+      gray: {
+        dark: "#737380",
+        medium: "#A8A8B3",
+        light: "#DBDCDD",
+      },
+      white: {
+        background: "#F8F8F8",
+        details: "#FEFEFE",
+      },
+    },
+  },
+};
