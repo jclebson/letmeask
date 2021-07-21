1a73a33b5bf341d5a23342a55bcac8215f15fb7c
Jefferson Clebson <jeffersonclebson@gmail.com>
2021-07-21 17:18:41 -0300

    Add Redux to Storybook and add Aside storie

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,23 @@
 import { addDecorator } from "@storybook/react";
 import { ThemeProvider } from "../src/styles/ThemeProvider";
-import { light } from "../src/styles/themes/light";
 import GlobalStyleComposed from "../src/styles/GlobalStyles";
+import { Provider } from "react-redux";
+import store from "../src/store/configureStore";
+import { light } from "../src/styles/themes/light";
+import { MemoryRouter as Router } from "react-router-dom";
 
-addDecorator((storyFn) => (
-  <>
-    <ThemeProvider theme={light}>
-      <GlobalStyleComposed />
-      {storyFn()}
-    </ThemeProvider>
-  </>
-));
+addDecorator((storyFn) => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={light}>
+          <GlobalStyleComposed />
+          {storyFn()}
+        </ThemeProvider>
+      </Router>
+    </Provider>
+  );
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
