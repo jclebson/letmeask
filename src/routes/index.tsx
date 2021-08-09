138f00e51c740e07947ad6a211c2e69b0166c409
Jefferson Clebson <jeffersonclebson@gmail.com>
2021-08-09 17:44:50 -0300

    Add admin and room pages with routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
 import { Switch, Route } from "react-router-dom";
 import { HomeRoute } from "./HomeRoute";
 import { NewRoomRoute } from "./NewRoomRoute";
-import { RoomRoute } from "./RoomRoute";
+import { AdminRoomRoute } from "./AdminRoomRoute";
+import { RoomPageRoute } from "./RoomPageRoute";
 
 export const AppRoutes = () => {
   return (
     <Switch>
       <Route path="/" exact component={HomeRoute} />
       <Route path="/rooms/new" component={NewRoomRoute} />
-      <Route path="/admin/room" component={RoomRoute} />
+      <Route path="/rooms/:id" component={RoomPageRoute} />
+      <Route path="/admin/room" component={AdminRoomRoute} />
     </Switch>
   );
 };
